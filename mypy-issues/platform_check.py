70007281a2c9d5540ab7748babfede7fb6fe3117
Stephen Rosen <sirosen@globus.org>
2021-06-30 21:03:57 +0000

    Try to reproduce windows-specific mypy issue

--- mypy-issues/platform_check.py
+++ mypy-issues/platform_check.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 
@@ -11,3 +12,13 @@ def bar():
     if sys.platform != "win32":
         return 1
     return 0
+
+
+def data_dir():
+    if sys.platform == "win32":
+        datadir = os.getenv("LOCALAPPDATA", os.getenv("APPDATA"))
+        if not datadir:
+            home = os.path.expanduser("~")
+            datadir = os.path.join(home, "AppData", "Local")
+        return os.path.join(datadir, "my-app")
+    return os.path.expanduser("~/.my-app")
