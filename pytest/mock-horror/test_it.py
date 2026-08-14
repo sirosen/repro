23f2138a6f81068e940545dc971e85dcec7cef01
Stephen Rosen <sirosen0@gmail.com>
2026-08-14 15:52:55 -0500

    Make it even more awful, and also assert the truth

--- pytest/mock-horror/test_it.py
+++ pytest/mock-horror/test_it.py
@@ -5,6 +5,9 @@ class Adder:
     def plus(self, a: int, b: int) -> int:
         return a + b
 
+    def double(self, a: int) -> int:
+        return a + a
+
 
 def test_patchy_stuff(monkeypatch):
     snek = Adder()
@@ -20,4 +23,21 @@ def test_patchy_stuff(monkeypatch):
 def test_abyssal_horror():
     snek = Adder()
 
-    assert snek.plus(1, 2) == 3  # wat
+    assert snek.plus(1, 2) == 0  # wat
+
+
+def test_the_horror_continues(monkeypatch):
+    snek = Adder()
+
+    with monkeypatch.context() as mp_context:
+        with mock.patch.object(Adder, "double", return_value=2):
+            assert snek.double(2) == 2
+
+            # uh-oh! it can happen here too!
+            mp_context.setattr(Adder, "double", lambda *args: 1)
+            assert snek.double(3) == 1
+
+        # you expected this, right?
+        assert snek.double(4) == 8
+
+    assert snek.double(5) == 2  # wat
